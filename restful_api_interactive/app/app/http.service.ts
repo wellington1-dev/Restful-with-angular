0361ea82bd1dab440134671d460733e3b159ef81
Wellington Silva <37451027+wellington1-dev@users.noreply.github.com>
2018-11-15 20:50:22 -0800

    updated

--- restful_api_interactive/app/app/http.service.ts
+++ restful_api_interactive/app/app/http.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable()
-  export class HttpService {
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
 
-    constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) { this.getTasks(); }
 
-    getTasks() {
+  getTasks() {
 
-      return this._http.get('/tasks');
-    }
-    getTask(id) {
-
-      return this._http.get('/tasks/' + id);
-    }
+    return this._http.get('/tasks');
   }
+
+}
